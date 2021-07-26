079bed2288803f2c39f72451dfaf4403c98aa594
AM2i9 <brennanpatrick20@gmail.com>
2021-07-26 15:13:57 -0400

    Created a document-based database system

--- bysondb/db.py
+++ bysondb/db.py
@@ -1,13 +1,21 @@
-from datetime import datetime
-from pathlib import Path
-from typing import Any, Optional
+from typing import Any
 
-import bson
+from bysondb.base import BysonDBBase
 
 
-class BysonDB:
+class BysonDB(BysonDBBase):
     """
-    A BysonDB instance.
+    A simple Key -> Value database.
+
+    Example
+    ----------
+    ```
+    my_db = BysonDB("my_db.bson")
+    my_db["first_name"] = "John"
+    my_db["age"] = 42
+
+    print(f"My name is {my_db["first_name"]}, and I am {my_db["age"]} years old")
+    ```
 
     Parameters
     -----------
@@ -18,58 +26,39 @@ class BysonDB:
     """
 
     def __init__(self, file: str):
-        
-        self.file = file
-        self.path = Path(file).with_suffix(".bson")
-
-        self._db = {}
-
-        self._load_if_exists()
-    
-    def __str__(self) -> str:
-        return f"<BysonDB file={self.path.resolve()} data={self._db}>"
-
-    def _load_if_exists(self) -> None:
-        """
-        Loads the database file if it exists. If it does not exist, a new one
-        is created.
-        """
-        try:
-            self._db = self._load()
-        except FileNotFoundError:
-            self._dump()
-
-    def _load(self) -> Optional[dict]:
-        """
-        Open the database file, and load the data stored in it.
-        """
-
-        byte_data = self.path.read_bytes()
-        data = bson.loads(byte_data)
-        
-        return data
-    
-    def _dump(self) -> None:
-        """
-        Open the database file, and dump the current data to it. If the file
-        does not exist, a new one is created.
-        """
-
-        if not self.path.exists():
-            self.path.resolve().parent.mkdir(parents=True, exist_ok=True)
-
-        data = bson.dumps(self._db)
-        self.path.write_bytes(data)
+        super().__init__(file, {})
 
     def __setitem__(self, key: str, value: dict) -> None:
 
         self._db[key] = value
         self._dump()
-    
+
     def __getitem__(self, key: str) -> Any:
         try:
             self._load()
             return self._db[key]
         except KeyError:
             return None
-        
+
+    def get_all(self) -> dict:
+        """
+        Returns a dict with all of the keys and their values.
+        """
+        self._load()
+        return self._db
+
+    def remove(self, key: str) -> None:
+        """
+        Remove a key from the database. Does nothing if the key does not exist.
+
+        Parameters
+        -----------
+        key: str
+            The key to remove from the database.
+        """
+        try:
+            del self._db[key]
+        except KeyError:
+            pass
+        else:
+            self._dump()
